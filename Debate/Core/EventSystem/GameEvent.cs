4fd2dcfbd693e29986f5363b4472450aeba6e1e4
Denny Scott <dennyscott301@gmail.com>
2016-08-10 15:45:56 -0500

    Added Testing Framework, Event System, and Event System Testing.

---
+++ Debate/Core/EventSystem/GameEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace Debate.Core.EventSystem
+{
+    public class GameEvent
+    {
+        public string Name;
+
+        public GameEvent()
+        {
+            Name = GetType().Name;
+        }
+    }
+}
